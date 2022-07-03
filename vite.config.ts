9d48338ec369718f2ef48d12e751ecd842365e95
Mehedi <mehedi@veefin.com>
2022-07-03 14:45:34 +0600

    Load app title from env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import {createHtmlPlugin} from 'vite-plugin-html'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+
+export default defineConfig((mode: any) => {
+    const env = loadEnv(mode, process.cwd());
+    return {
+        plugins: [
+            vue(),
+            createHtmlPlugin({
+                minify: true,
+                inject: {
+                    data: {
+                        title: env.VITE_APP_TITLE ?? 'Vite App',
+                    }
+                }
+            })
+        ]
+    }
 })
